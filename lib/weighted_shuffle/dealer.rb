1352f3956eb4f57f05107a8a571aaa96cec99923
schasse <sebastian.schasse@gapfish.com>
2014-01-29 11:53:09 +0100

    add option factor

--- lib/weighted_shuffle/dealer.rb
+++ lib/weighted_shuffle/dealer.rb
@@ -3,13 +3,17 @@ require 'active_support/core_ext/enumerable'
 module WeightedShuffle
   class Dealer < Struct.new(:array)
 
-    def weighted_shuffle
+    def weighted_shuffle(options = {})
+      factor = options.delete(:factor)
       a = Marshal.load Marshal.dump(array)
-      sum = a.sum { |k,v| v || 0.0 }
+      unless factor.nil?
+        a.map! { |k, v| [k, v**factor] }
+      end
+      sum = a.sum { |k, v| v || 0.0 }
       b = []
       a.length.times do
         random = SecureRandom.random_number * sum
-        a.each_with_index do |(k,v),j|
+        a.each_with_index do |(k, v), j|
           if random <= v
             b << a.delete_at(j).first
             sum -= v
--- spec/weighted_shuffle/dealer_spec.rb
+++ spec/weighted_shuffle/dealer_spec.rb
@@ -3,35 +3,49 @@ require 'spec_helper'
 module WeightedShuffle
   describe Dealer do
     describe '#weighted_shuffle' do
-      [[ [[:a, 1], [:b, 2]], [:a, :b] ],
-       [ [[1, 1], [2, 3]], [1, 2] ],
-       [ [[1, 1], [2, 0]], [1, 2] ],
-       [ [[1, 1], [2, 0], [3, 0]], [1, 2, 3] ],
-       [ [[2.0, 1], [:a, 1.5], ['a', 2.3]], [2.0, :a, 'a'] ]]
-        .each do |input, output|
 
-        it "returns a permutation of #{output} given #{input}" do
-          output.permutation.to_a.should include(
-            Dealer.new(input).weighted_shuffle)
-        end
+      context 'without factor' do
+        [[ [[:a, 1], [:b, 2]], [:a, :b] ],
+          [ [[1, 1], [2, 3]], [1, 2] ],
+          [ [[1, 1], [2, 0]], [1, 2] ],
+          [ [[1, 1], [2, 0], [3, 0]], [1, 2, 3] ],
+          [ [[2.0, 1], [:a, 1.5], ['a', 2.3]], [2.0, :a, 'a'] ]]
+          .each do |input, output|
 
-        it 'gives correct relative frequency for first position' do
-          weights = input.map(&:last)
-          # calculate probabiities
-          sum = weights.sum
-          probabilities = weights.map { |e| e.to_f / sum }
+          it "returns a permutation of #{output} given #{input}" do
+            expect(output.permutation.to_a).to include(
+              Dealer.new(input).weighted_shuffle)
+          end
 
-          # establish frequencies
-          size = 1000
-          sample = size.times.map { Dealer.new(input).weighted_shuffle }
-          frequencies = input.map(&:first)
-            .map do |e|
+          it 'gives correct relative frequency for first position' do
+            weights = input.map(&:last)
+            # calculate probabiities
+            sum = weights.sum
+            probabilities = weights.map { |e| e.to_f / sum }
+
+            # establish frequencies
+            size = 1000
+            sample = size.times.map { Dealer.new(input).weighted_shuffle }
+            frequencies = input.map(&:first)
+              .map do |e|
               sample.select { |s| s.first == e }.count.to_f / size
             end
 
-          # calculate sum of absolute values of differences
-          diff = probabilities.zip(frequencies).map { |p,f| (p - f).abs }.sum
-          diff.should < 0.1
+            # calculate sum of absolute values of differences
+            diff = probabilities.zip(frequencies).map { |p,f| (p - f).abs }.sum
+            expect(diff).to < 0.1
+          end
+        end
+      end
+
+      context 'with factor' do
+        let(:input) { [[1, 1], [2, 1], [3, 1.01]] }
+        let(:factor) { 100_000 }
+
+        it 'big factor has a big impact' do
+          expect(1.01**factor).to eq(Float::INFINITY)
+          sample = Dealer.new(input).weighted_shuffle factor: factor
+          expect(sample.first).to eq(3)
         end
       end
     end
