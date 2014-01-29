7a5b59577fb82d510c3f498dc05ff04a5d14e8c9
schasse <sebastian.schasse@gapfish.com>
2014-01-29 17:32:56 +0100

    comments and another spec

--- spec/weighted_shuffle/dealer_spec.rb
+++ spec/weighted_shuffle/dealer_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 module WeightedShuffle
   describe Dealer do
@@ -21,31 +22,43 @@ module WeightedShuffle
             weights = input.map(&:last)
             # calculate probabiities
             sum = weights.sum
-            probabilities = weights.map { |e| e.to_f / sum }
+            probabilities = weights.map { |weight| weight.to_f / sum }
 
             # establish frequencies
             size = 1000
-            sample = size.times.map { Dealer.new(input).weighted_shuffle }
-            frequencies = input.map(&:first)
-              .map do |e|
-              sample.select { |s| s.first == e }.count.to_f / size
+            samples = size.times.map { Dealer.new(input).weighted_shuffle }
+            frequencies = input.map(&:first).map do |e|
+              samples.select { |s| s.first == e }.count.to_f / size
             end
 
             # calculate sum of absolute values of differences
             diff = probabilities.zip(frequencies).map { |p,f| (p - f).abs }.sum
-            expect(diff).to < 0.1
+            expect(diff).to be < 0.1
           end
         end
       end
 
       context 'with factor' do
-        let(:input) { [[1, 1], [2, 1], [3, 1.01]] }
-        let(:factor) { 100_000 }
 
         it 'big factor has a big impact' do
-          expect(1.01**factor).to eq(Float::INFINITY)
-          sample = Dealer.new(input).weighted_shuffle factor: factor
+          input = [[1, 1], [2, 1], [3, 1.01]]
+          sample = Dealer.new(input).weighted_shuffle factor: 100_000
           expect(sample.first).to eq(3)
+        end
+
+        it 'gives expected relative frequency ' do
+          input = [[:a, 1], [:b, 2]]
+          size = 1000
+          samples = size.times.map do
+            Dealer.new(input).weighted_shuffle factor: 2
+          end
+          frequencies = input.map(&:first).map do |e|
+            samples.select { |sample| sample.first == e }.count.to_f / size
+          end
+
+          diff = [1.0 / 5, 4.0 / 5].zip(frequencies).map { |p, f| (p - f).abs }
+            .sum
+          expect(diff).to be < 0.1
         end
       end
     end
